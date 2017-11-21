713785997b231bef65fa2d66c2263fe8cb0d0dc5
Raghul Shanmuganathan <raghul.shanmuganathan06@gmail.com>
2017-11-21 16:28:36 +0100

    Update robotex.py

--- Telerobotics/robotex.py
+++ Telerobotics/robotex.py
@@ -5,7 +5,13 @@ import serial
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import math as m
 
+acc_x=0
+acc_y=0
+acc_z=0
+
+mpu=mpu6050(0x68)
 
 t=time.time()
 
@@ -31,6 +37,26 @@ def ser_readData():
 		#		flag=1
 	return index,val,time.time()-t
 
+
+def imu_readdata():
+	#Insert from Rpi
+	# Convert accel data to roll,pitch and yaw angles
+	acc=np.sqrt(Gpx**2+Gpy**2+Gpz**2)
+	r_acc=np.rad2deg(m.atan2(Gpy,Gpz))
+	p_acc=np.rad2deg(m.atan2(-Gpx,np.sqrt(Gpy**2+Gpz**2)))
+	diff=acc-acc_past
+	acc_past=acc
+	# Convert gyro data to roll,pitch and yaw angles
+	r_gyr=r_past+(x_gyr*Ts)
+	p_gy=p_past+(y_gyr*Ts)
+	
+	sig=0.4								#Normal Distribution parameters
+	mu=1
+	alpha=(1/(sig*(np.sqrt(2*np.pi))))*(np.exp(-1*((acc-mu)**2)/(2*(sig**2))))
+	
+	r_now=(r_gyr*alpha)+(r_acc*(1-alpha))
+	p_now=(p_gyr*alpha)+(p_acc*(1-alpha))
+	
 if __name__=='__main__':
 #	ser_init()
 	print ser_readData()
